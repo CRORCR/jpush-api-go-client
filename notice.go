0761df0f9c7fc52766a92bc2ddc3fd54262d5195
lichangquan <lichangquan@createcloud.com>
2020-08-28 16:44:29 +0800

    add .

--- notice.go
+++ notice.go
@@ -1,21 +1,26 @@
 package jpushclient
 
 type Notice struct {
-	Alert    string          `json:"alert,omitempty"`
+	Alert    Alert           `json:"alert,omitempty"`
 	Android  *AndroidNotice  `json:"android,omitempty"`
 	IOS      *IOSNotice      `json:"ios,omitempty"`
 	WINPhone *WinPhoneNotice `json:"winphone,omitempty"`
 }
 
+type Alert struct {
+	Title    string `json:"title"`
+	Subtitle string `json:"subtitle"`
+	Body     string `json:"body"`
+}
 type AndroidNotice struct {
-	Alert     string                 `json:"alert"`
+	Alert     Alert                  `json:"alert"`
 	Title     string                 `json:"title,omitempty"`
 	BuilderId int                    `json:"builder_id,omitempty"`
 	Extras    map[string]interface{} `json:"extras,omitempty"`
 }
 
 type IOSNotice struct {
-	Alert            interface{}            `json:"alert"`
+	Alert            Alert                  `json:"alert"`
 	Sound            string                 `json:"sound,omitempty"`
 	Badge            string                 `json:"badge,omitempty"`
 	ContentAvailable bool                   `json:"content-available,omitempty"`
@@ -25,13 +30,13 @@ type IOSNotice struct {
 }
 
 type WinPhoneNotice struct {
-	Alert    string                 `json:"alert"`
+	Alert    Alert                  `json:"alert"`
 	Title    string                 `json:"title,omitempty"`
 	OpenPage string                 `json:"_open_page,omitempty"`
 	Extras   map[string]interface{} `json:"extras,omitempty"`
 }
 
-func (this *Notice) SetAlert(alert string) {
+func (this *Notice) SetAlert(alert Alert) {
 	this.Alert = alert
 }
 
